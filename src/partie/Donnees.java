1ef3a8617d3bf6de090f86122c3b2702cb8857f6
Virgil <virgil031299@gmail.com>
2018-04-05 11:49:07 +0200

    Etape 2 :
    + Donnees.java
    ~ Donnees (getters)

---
+++ src/partie/Donnees.java
@@ -0,0 +1,75 @@
+package partie;
+
+import graphe.Graphe;
+import stratégie.Strategie;
+
+public class Donnees implements DonneesJoueur{
+	private int caisse;
+	private Graphe graphe;
+	private String nom;
+	private int qualite;
+	private Realisation realisation;
+	private Strategie strategie;
+	
+	
+	@Override
+	public void actualisation(int temps) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void baisseQualite(int detlta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depense(int somme) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getCaisse() {
+		// TODO Auto-generated method stub
+		return this.caisse;
+	}
+
+	@Override
+	public Graphe getGraphe() {
+		// TODO Auto-generated method stub
+		return this.graphe;
+	}
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return this.nom;
+	}
+
+	@Override
+	public int getQualite() {
+		// TODO Auto-generated method stub
+		return this.qualite;
+	}
+
+	@Override
+	public Realisation getRealisation(String id) {
+		// TODO Auto-generated method stub
+		return this.realisation;
+	}
+
+	@Override
+	public Strategie getStrategie() {
+		// TODO Auto-generated method stub
+		return this.strategie;
+	}
+
+	@Override
+	public void uneSemaine() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
