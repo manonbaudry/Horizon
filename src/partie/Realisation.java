683983f7aee990dfbf87e106ab3cc77caf275a76
Dubois Guillaume <guillaume.dubois@etudiant.univ-lille1.fr>
2018-04-13 11:48:16 +0200

    realisation

--- src/partie/Realisation.java
+++ src/partie/Realisation.java
@@ -1,5 +1,13 @@
 package partie;
 
+import java.util.HashMap;
+
+import description.Couleur;
+
 public class Realisation {
 	
+	private int duree_reelle;
+	private boolean acceleration;
+	private HashMap<Couleur,Boolean> protections;
+	private Etat etat;
 }
