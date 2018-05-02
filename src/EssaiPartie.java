00f0053da76fb7bee9111f3076079b246a98ef76
Manon <Manon@Manou>
2018-05-02 10:29:59 -0700

    --Etape 2

---
+++ src/EssaiPartie.java
@@ -0,0 +1,12 @@
+import partie.*;
+import strategie.*;
+
+public class EssaiPartie {
+
+	public static void main(String [] args) {
+		Donnees joueur = new Donnees("j1", new JoueurSimple());
+		joueur.getStrategie().jouerEtape(joueur);
+		
+		
+		}
+}
--- src/partie/Donnees.java
+++ src/partie/Donnees.java
@@ -83,8 +83,7 @@ public class Donnees implements DonneesJoueur, VueJoueur{
 			}
 		}
 		actualisation();
-		numeroTour++;
-		
+		numeroTour++;	
 	}
 
 	@Override
