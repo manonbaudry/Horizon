cb2b7d98743195e80437f8a1e82249948a38e3c0
Manon <Manon@Manou>
2018-05-03 10:18:03 -0700

    --Etape 2 Création énum Tour

---
+++ src/partie/Tour.java
@@ -0,0 +1,17 @@
+package partie;
+
+public enum Tour {
+	/**
+	 * Le joueur décide des protections et accélérations à acheter pour les étapes suivantes
+	 */
+	JALON, 
+	/**
+	 * Une question est posée au joueur pour lui permettre de gagner de l'argent
+	 */
+	QUIZZ,
+	/**
+	 * tirage d'un aléa
+	 */
+	ALEA;
+	
+}
